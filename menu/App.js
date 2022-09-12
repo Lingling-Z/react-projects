bc9b05d400cfdb6712ca037085806ff2d4ac5e76
Lingling-Z <97594629+Lingling-Z@users.noreply.github.com>
2022-09-12 21:09:41 +1200

    initial commit

---
+++ menu/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items)
+  const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+
+    const filteredItems = items.filter((item) => item.category === category)
+    setMenuItems(filteredItems)
+  }
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={allCategories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
+}
+
+export default App
